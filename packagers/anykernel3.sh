c45bbbada854cb10bd29eb869e8a9e91cca9c2e9
Nick <nift4@protonmail.com>
2021-10-21 10:23:00 +0200

    add ak3 packager

---
+++ packagers/anykernel3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Pack the kernel into an AnyKernel3 ZIP
+# Required variables:
+#  AK3_ZIP_NAME - zip name of resulting zip
+#  AK3_REPO_URL - full git repository url
+#  AK3_REPO_BRANCH - full git repository url
+rm -rf "${WORKSPACE}/anykernel3_out"
+mkdir "${WORKSPACE}/anykernel3_out"
+rm -rf "${WORKSPACE}/anykernel3_tmp"
+git clone "${AK3_REPO_URL}" --depth 1 -b "${AK3_REPO_BRANCH}" "${WORKSPACE}/anykernel3_tmp"
+cd "${WORKSPACE}/anykernel3_tmp"
+cp -r "$PKG_OUTDIR/*" .
+zip -r9 "${WORKSPACE}/anykernel3_out/${AK3_ZIP_NAME}" * -x .git README.md *placeholder
+cd - >/dev/null
+export PKG_OUTDIR="${WORKSPACE}/anykernel3_out"

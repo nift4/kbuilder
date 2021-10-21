184cc457139e82935c723c0717302d9e107bf932
Nick <nift4@protonmail.com>
2021-10-21 10:41:43 +0200

    fix ak3 error

--- packagers/anykernel3.sh
+++ packagers/anykernel3.sh
@@ -11,7 +11,7 @@ mkdir "${WORKSPACE}/anykernel3_out"
 rm -rf "${WORKSPACE}/anykernel3_tmp"
 git clone "${AK3_REPO_URL}" --depth 1 -b "${AK3_REPO_BRANCH}" "${WORKSPACE}/anykernel3_tmp"
 cd "${WORKSPACE}/anykernel3_tmp"
-cp -r "$PKG_OUTDIR/*" .
+cp -r $PKG_OUTDIR/* .
 zip -r9 "${WORKSPACE}/anykernel3_out/${AK3_ZIP_NAME}" * -x .git README.md *placeholder
 if [ "x$AK3_KEEP_OUTDIR" == "xtrue" ]; do
   cp -r "$PKG_OUTDIR/*" "${WORKSPACE}/anykernel3_out/"

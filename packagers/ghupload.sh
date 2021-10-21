c1423192754c9544c062a6c5017e885f9acd1a10
Nick <nift4@protonmail.com>
2021-10-21 10:12:36 +0200

    small fixup

--- packagers/ghupload.sh
+++ packagers/ghupload.sh
@@ -8,6 +8,6 @@
 #  Branch - kernel branch
 
 releaseId=$($(curl --user "$GH_USER:$GH_TOKEN" -X POST https://api.github.com/repos/${GH_REPO_USER}/${GH_REPO}/releases -d "{\"tag_name\": \"$(date +%4Y%m%d%H%M)\",\"target_commitish\": \"$Branch\",\"name\": \"$(date +%4Y%m%d%H%M) Kernel\",\"body\": \"Automatically built.\",\"draft\": false,\"prerelease\": false}") | python -c 'import json,sys;print(json.load(sys.stdin)["id"])')
-for file_name in $(find "$OUTDIR" -type f); do
+for file_name in $(find "$PKG_OUTDIR" -type f); do
   curl --user "$GH_USER:$GH_TOKEN" -X POST https://uploads.github.com/repos/${GH_REPO_USER}/${GH_REPO}/releases/${releaseId}/assets?name=$(basename "${file_name}") --header 'Content-Type: application/octet-stream' --upload-file ${file_name}
 done

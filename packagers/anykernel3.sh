1b0283dbd6a17722e1a94a5466b091025b22bc11
Nick <nift4@protonmail.com>
2021-10-21 10:39:05 +0200

    packagers: anykernel3: add keep_outdir

--- packagers/anykernel3.sh
+++ packagers/anykernel3.sh
@@ -4,6 +4,8 @@
 #  AK3_ZIP_NAME - zip name of resulting zip
 #  AK3_REPO_URL - full git repository url
 #  AK3_REPO_BRANCH - full git repository url
+# Optional variables:
+#  AK3_KEEP_OUTDIR - copy files into new outdir to upload them
 rm -rf "${WORKSPACE}/anykernel3_out"
 mkdir "${WORKSPACE}/anykernel3_out"
 rm -rf "${WORKSPACE}/anykernel3_tmp"
@@ -11,5 +13,8 @@ git clone "${AK3_REPO_URL}" --depth 1 -b "${AK3_REPO_BRANCH}" "${WORKSPACE}/anyk
 cd "${WORKSPACE}/anykernel3_tmp"
 cp -r "$PKG_OUTDIR/*" .
 zip -r9 "${WORKSPACE}/anykernel3_out/${AK3_ZIP_NAME}" * -x .git README.md *placeholder
+if [ "x$AK3_KEEP_OUTDIR" == "xtrue" ]; do
+  cp -r "$PKG_OUTDIR/*" "${WORKSPACE}/anykernel3_out/"
+fi
 cd - >/dev/null
 export PKG_OUTDIR="${WORKSPACE}/anykernel3_out"
